92e9813b0f3e5afe464f0c137bdd95d10bb34cd8
John Lloyd Paranal <john.paranal@novare.com.hk>
2023-05-23 14:17:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import App from './App';
 import ProductProvider from './contexts/ProductContext';
 import SidebarProvider from './contexts/SidebarContext';
 import CartProvider from './contexts/CartContext';
+import { Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SidebarProvider>
-      <CartProvider>
-        <ProductProvider>
-          <App />
-        </ProductProvider>
-      </CartProvider>
-    </SidebarProvider>
+    <Router>
+      <SidebarProvider>
+        <CartProvider>
+          <ProductProvider>
+            <App />
+          </ProductProvider>
+        </CartProvider>
+      </SidebarProvider>
+    </Router>
   </React.StrictMode>
 );
