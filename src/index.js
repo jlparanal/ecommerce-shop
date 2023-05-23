2ed3ef2876ede89d4922bc014343b465f46e27c0
John Lloyd Paranal <john.paranal@novare.com.hk>
2023-05-23 14:19:19 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import ProductProvider from './contexts/ProductContext';
+// import SidebarProvider from './contexts/SidebarContext';
+// import CartProvider from './contexts/CartContext';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <SidebarProvider>
+//     <CartProvider>
+//       <ProductProvider>
+//         <React.StrictMode>
+//           <App />
+//         </React.StrictMode>
+//       </ProductProvider>
+//     </CartProvider>
+//   </SidebarProvider>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ProductProvider from './contexts/ProductContext';
-import SidebarProvider from './contexts/SidebarContext';
-import CartProvider from './contexts/CartContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ProductProvider } from './contexts/ProductContext';
+import { SidebarProvider } from './contexts/SidebarContext';
+import { CartProvider } from './contexts/CartContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <SidebarProvider>
-    <CartProvider>
-      <ProductProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </ProductProvider>
-    </CartProvider>
-  </SidebarProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <SidebarProvider>
+        <CartProvider>
+          <ProductProvider>
+            <App />
+          </ProductProvider>
+        </CartProvider>
+      </SidebarProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
