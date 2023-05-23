44d5ae6f618e65c446b7fe762812b3d1eaad7241
John Lloyd Paranal <john.paranal@novare.com.hk>
2023-05-23 12:30:15 +0800

    ecommerce shop

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import ManImg from '../img/man-hero.png';
+import {Link} from 'react-router-dom';
+
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
+      <div className='container mx-auto flex justify-around h-full'>
+        <div className='flex flex-col justify-center'>
+          <div className='font-semibold flex items-center uppercase'>
+            <div className='w-10 h-[2px] bg-orange-500 mr-3'></div>New Trend
+          </div>
+          <h1 className='text-[70px] leading-[1.1] font-light mb-4'>AUTUM SALE STYLISH 
+          <br/> <span className='font-semibold'>MENS</span></h1>
+          <Link 
+          to={'/'} 
+          className='self-start uppercase font-semibold border-b-2 border-primary'>
+          Discover More</Link>
+        </div>
+        <div className='hidden lg:block'>
+          <img className='h-[700px] w-[400px] object-cover w-full h-full' src={ManImg} alt='' />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
